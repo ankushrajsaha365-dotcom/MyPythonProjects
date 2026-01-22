121c4afa4958f38e58a45b422b99a87cafaccaf9
Ankush Raj Saha <ankushrajsaha365@gmail.com>
2026-01-22 20:02:58 +0530

    standard modules

---
+++ modules/basics_/mod_os.py
@@ -0,0 +1,7 @@
+import os
+
+# print(os.getcwd())
+# os.mkdir("test_folder")
+# os.remove("file.txt")
+
+print(os.name)
---
+++ modules/basics_/mod_random.py
@@ -0,0 +1,5 @@
+import random
+
+print(random.randint(1, 10))
+print(random.choice([1, 2, 3, 4]))
+random.shuffle([10, 20, 30])
