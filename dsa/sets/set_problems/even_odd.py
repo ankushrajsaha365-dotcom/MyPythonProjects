7535b41a62174dbf33e88e0ef48008c828ff3493
Ankush Raj Saha <ankushrajsaha365@gmail.com>
2026-01-26 23:12:50 +0530

    updated sets

--- dsa/sets/set_problems/even_odd.py
+++ dsa/sets/set_problems/even_odd.py
@@ -16,3 +16,14 @@ for i in my_list:
 
 print("Even set:",even)
 print("Odd set:",odd)
+
+
+
+
+# numbers = list(map(int, input().split()))
+
+# even = {x for x in numbers if x % 2 == 0}
+# odd = {x for x in numbers if x % 2 != 0}
+
+# print("Even set:", even)
+# print("Odd set:", odd)
